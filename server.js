f636c546d33742e942a9dba1f9ac6073b673e00d
Brady Peterson <bpeterson1663@gmail.com>
2021-01-16 20:21:46 -0600

    feat(graphql): first graphql schema

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            },
+        })
+    })
+})
+
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
 
 app.listen(5000, () => console.log("server running"))
